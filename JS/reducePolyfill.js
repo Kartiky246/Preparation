107e4885a138a0bcf2476404dade0662c9a42cf8
kartiky246 <kartiky246@gmail.com>
2023-11-21 23:33:38 +0530

    changes

--- JS/reducePolyfill.js
+++ JS/reducePolyfill.js
@@ -1,11 +1,11 @@
-const arr = [1,2,3,4,5,6,7,8,9,5];
+const arr = [0,2,3,4,5,6,7,8,9,5];
 
 Array.prototype.myOwnReduce = function(fn,initalValue){
     let value = initalValue;
     for(let i =0; i<this.length;i++){
-        value = value ? fn(value,this[i],i,this) : this[0]
+        value =  fn(value,this[i],i,this) 
     }
     return value
 }
 
-console.log(arr.myOwnReduce((acc,curr)=> acc+curr,0))
+console.log(arr.myOwnReduce((acc,curr)=> acc*curr,0))
