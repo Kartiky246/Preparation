63fa16df0516c8c035a9d41ae0ef18fd9bd87dcc
kartiky246 <kartiky246@gmail.com>
2025-01-17 23:17:49 +0530

    fix(): reduce polyfill update

--- JS/reducePolyfill.js
+++ JS/reducePolyfill.js
@@ -1,11 +1,15 @@
 const arr = [0,2,3,4,5,6,7,8,9,5];
 
-Array.prototype.myOwnReduce = function(fn,initalValue){
-    let value = initalValue;
-    for(let i =0; i<this.length;i++){
-        value =  fn(value,this[i],i,this) 
+Array.prototype.myReduce = function(fn,initialValue){
+    if(initialValue===undefined && this.length===0){
+        throw new TypeError("Reduce of empty array with no initial value");
     }
-    return value
+    if(initialValue===undefined) initialValue=this[0];
+    let res = initialValue;
+    for(let i = res===initialValue?1:0; i<this.length;i++){
+        res = fn(res,this[i],i,this);
+    }
+    return res;
 }
 
 console.log(arr.myOwnReduce((acc,curr)=> acc+curr))
--- test/test.js
+++ test/test.js
@@ -1,1 +1,26 @@
-console.log("HI")
+
+
+const click = () =>{
+    console.log('click');
+}
+
+const obj = {
+    value: 42,
+    logValue: function() {
+      console.log(this.value,);
+    }
+  };
+  
+
+  function debounce(func, timeout = 300){
+    let timer;
+    return function(...args){
+      clearTimeout(timer);
+      timer = setTimeout(func.bind(this, ...args), timeout);
+    };
+  }
+
+const myClick = debounce(obj.logValue);
+obj.logValue();
+
+myClick();
