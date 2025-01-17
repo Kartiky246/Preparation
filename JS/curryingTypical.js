0c2d954549adc3408a10f1da90a90d182e03d36e
kartiky246 <kartiky246@gmail.com>
2025-01-18 02:37:37 +0530

    fix(): currying typical code update

--- JS/curryingTypical.js
+++ JS/curryingTypical.js
@@ -1,15 +1,10 @@
 const max = 5;
 
-const sum = (...args) =>{
-    if(args.length===max) return args.reduce((acc,curr)=>acc+curr);
-    const solve = (...args2) =>{
-        args = args.concat(args2);
-        if(args.length===max) return args.reduce((acc,curr)=>acc+curr)
-        return solve
-    }
-    return solve
-}
 
+function sum(...a){
+    if(a.length===max) return a.reduce((acc,curr)=>acc+curr,0);
+    return (...b)=> sum(...a,...b)
+}
 console.log(sum(1,2,3,4,5))
 console.log(sum(1,2,3,4)(5))
 console.log(sum(1,2,3)(4)(5))
