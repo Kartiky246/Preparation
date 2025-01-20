2bbb972ed910853819ca4510f39ea0a53ba940db
kartiky246 <kartiky246@gmail.com>
2025-01-20 20:21:50 +0530

    fix(): debounce code update

--- JS/debouncePollyFill.js
+++ JS/debouncePollyFill.js
@@ -1,50 +1,27 @@
-const bt = document.querySelector(".btn");
-const counter = document.querySelector(".counter");
 
-let btnPressed = 0;
-let counterValue = 0;
-bt.addEventListener('click',()=>{
-	btnPressed++;
-	bt.innerText = 'press ' + btnPressed
-	inc()
-})
-
-function increaseCounter(){
-	counterValue++;
-	counter.innerText = counterValue;
+const customDebounce = (callback,time, immediate, context=this)=>{
+    let timer;
+    return (...args)=>{
+        let callNow = immediate && !timer;
+        clearTimeout(timer);
+        timer = setTimeout(()=>{
+            timer = null;
+            if(!immediate){
+                callback.apply(context,args);
+            }
+        },time)
+        if(callNow){
+            callback.apply(context,args);
+        }
+    } 
 }
 
+const greet = (name)=>console.log(`Hi ${name}`);
 
-const debounce = (fn,delay) => {
-	let timer;
-	return (...args) => {
-		clearTimeout(timer);
-		timer = setTimeout(()=>{
-			fn.apply(this,args)
-		},delay)
-	}
-}
+const debounceGreet = customDebounce(greet,400,true);
 
-const inc = debounce(increaseCounter, 1000)
-
-
-
-// html and css - 
-
-
-
-// <div class="container">
-// 	<button class='btn'>
-// 	Press
-// 	</button>
-// 	<span class="counter">0</span>
-// </div>
-
-
-// .container{
-// 	display:flex;
-// 	flex-direction:column;
-// 	justify-content:center;
-// 	align-items:center;
-// 	gap:50px;
-// }
+debounceGreet('Kartik');
+debounceGreet('a');
+debounceGreet('b');
+debounceGreet('c');
+setTimeout(()=>debounceGreet('d'),400)
