a04caf678804e82a8804e9f8c7f17f17aa054615
kartiky246 <kartiky246@gmail.com>
2025-12-28 14:52:35 +0530

    feat(): add lld problem statement

--- LLD_machine_coding/questions/featureFlagComponent.ts
+++ LLD_machine_coding/questions/featureFlagComponent.ts
@@ -6,6 +6,9 @@
 // Create a React component using context provider and wrap the app in it
 // In that component declare the context and update the values of keys by making BE api calls
 // Create a Feature component which will take feature key and children to render the UI conditionally based and feature flag,
-// this component will use useContext and do the conditional rendering
+// this component will consume context and do the conditional rendering
 
-// NOTE: feature object can be in store also like redux or zustand
+// NOTE: we can store feature object in global state also using redux or zustand. Using a separate component (Feature)
+//  to do conditional rendering still apply there
+
+// Refer FeatureFlagProvider.tsx and Feature.ts for solution
