58a638d58c21a47d76cdfeb29adfab6feb190dcb
kartiky246 <kartiky246@gmail.com>
2026-01-08 21:48:17 +0530

    feat(): add singleton design pattern

--- LLD_machine_coding/app/DESIGN_PATTERNS/singleton.ts
+++ LLD_machine_coding/app/DESIGN_PATTERNS/singleton.ts
@@ -3,14 +3,41 @@
 // class controls it's instantiation by itslef and consumer can't instantiate it (this can be achieve by making constructor private)
 // a global public access of the instance will be given by using factory method 
 
-export class Singleton{
-    private static _instance: Singleton;
-    private constructor(){}
 
-    static get instance(): Singleton{
-        if(!Singleton._instance){
-            Singleton._instance = new Singleton();
-         }
-        return Singleton._instance;
-    }
+// Below is the class level implementation of Singleton pattern
+// export class Singleton{
+//     private static _instance: Singleton;
+//     private constructor(){}
+
+//     static get instance(): Singleton{
+//         if(!Singleton._instance){
+//             Singleton._instance = new Singleton();
+//          }
+//         return Singleton._instance;
+//     }
+// }
+
+// In frontend codebase, ESM module are preferd for enforcing Singleton pattern
+// Important:
+//ES modules execute once
+
+// Exports are cached
+
+// Imports are live references
+
+// data is Module scope, ie. private shared state
+
+// JS engine builds module graph
+
+// Each module is evaluated once
+
+// Results are cached
+
+// All future imports reuse the cache
+
+// So we can simply export new Singleton() and avoid using static getter to access instance
+
+
+export const Singleton = {
+    value: 1
 }
--- LLD_machine_coding/app/src/App.tsx
+++ LLD_machine_coding/app/src/App.tsx
@@ -2,13 +2,10 @@ import './App.css'
 import { Feature } from './shared/components/Feature'
 import { FeatureTypes } from './shared/constants/Features'
 import UseState from './shared/hooks/UseState'
-import {Singleton} from "../DESIGN_PATTERNS/singleton";
 
 
 
 function App() {
-   const a = Singleton.instance;
-   debugger;
   return (
    <div className='app'>
       <Feature featureKey={FeatureTypes.COUNTER_BUTTON}>
